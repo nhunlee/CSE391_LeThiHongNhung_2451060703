83d304a332404a89be4b5ac6287d7b0ff5e43cb6
nhunle <nhung16011999@gmail.com>
2026-05-30 20:58:01 +0700

    Hoàn thành bài tập A1

--- PBT_07/var_let_const.js
+++ PBT_07/var_let_const.js
@@ -1,10 +1,5 @@
-// ==========================================
-// FILE: var_let_const.js
-// CÂU A1 - Kiểm chứng kết quả var, let, const
-// ==========================================
-
 console.log("--- Đoạn 1: var hoisting ---");
-console.log(x); // Kết quả: undefined
+console.log(x); 
 var x = 5;
 
 console.log("\n--- Đoạn 2: let TDZ ---");
@@ -13,7 +8,6 @@ try {
     let y = 10;
 } catch (error) {
     console.log("Gặp lỗi:", error.name, "-", error.message); 
-    // Kết quả: ReferenceError - Cannot access 'y' before initialization
 }
 
 console.log("\n--- Đoạn 3: const re-assignment ---");
@@ -23,18 +17,16 @@ try {
     console.log(z);
 } catch (error) {
     console.log("Gặp lỗi:", error.name, "-", error.message); 
-    // Kết quả: TypeError - Assignment to constant variable.
 }
 
 console.log("\n--- Đoạn 4: const với Reference Type (Array) ---");
 const arr = [1, 2, 3];
 arr.push(4);
-console.log(arr); // Kết quả: [ 1, 2, 3, 4 ]
+console.log(arr); 
 
 console.log("\n--- Đoạn 5: Block Scope ---");
 let a = 1;
 {
     let a = 2;
-    console.log("Trong block:", a); // Kết quả: 2
 }
-console.log("Ngoài block:", a); // Kết quả: 1
+console.log("Ngoài block:", a); 
